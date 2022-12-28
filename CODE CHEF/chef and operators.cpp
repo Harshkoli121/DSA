11b5a527647469bb23592a77b1bf5e7d500d14ee
Harsh Koli <113970702+Harshkoli121@users.noreply.github.com>
2022-12-28 19:54:03 +0530

    Create chef and operators.cpp

---
+++ CODE CHEF/chef and operators.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	long int t;
+	cin>>t;
+	while(t--){
+	    long int a,b;
+	    cin>>a>>b;
+	    if(a<b)
+	    {
+	    cout<<"<"<<endl;
+	    }else if(a>b)
+	    {
+	        
+	            cout<<">"<<endl;
+	        }else
+	            
+	                cout<<"="<<endl;
+	            
+	        }
+	    
+	
+	return 0;
+}
