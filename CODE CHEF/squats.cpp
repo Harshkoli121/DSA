0a424bde4624d151303e15c7bbf1c4ff9b699f86
Harsh Koli <113970702+Harshkoli121@users.noreply.github.com>
2022-12-29 21:26:27 +0530

    Create squats.cpp

---
+++ CODE CHEF/squats.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a;
+	    cin>>a;
+	    cout<<a*15<<endl;
+	}
+	return 0;
+}
