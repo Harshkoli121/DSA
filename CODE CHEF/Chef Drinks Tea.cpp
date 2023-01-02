d3bf3d438b183d185a522f92cebc4a9240747fe1
Harsh Koli <113970702+Harshkoli121@users.noreply.github.com>
2023-01-02 22:25:23 +0530

    Create Chef Drinks Tea.cpp

---
+++ CODE CHEF/Chef Drinks Tea.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y,z;
+	    cin>>x>>y>>z;
+	    if(x<=y){
+	        cout<<z<<endl;
+	    }else if(x>y && x%y==0){
+	        cout<<(x/y)*z<<endl;
+	    }else{
+	        cout<<(x/y+1)*z<<endl;
+	    }
+}
+return 0;
+}
