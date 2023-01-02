472f73e044c1526645dc3a94bf516267b2cfca5a
Harsh Koli <113970702+Harshkoli121@users.noreply.github.com>
2023-01-02 21:58:00 +0530

    Create Ranklist pages.cpp

---
+++ CODE CHEF/Ranklist pages.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x%25<=0){
+	        cout<<x/25<<endl;
+	    }else{
+	        cout<<x/25+1<<endl;
+	    }
+	}
+	return 0;
+}
