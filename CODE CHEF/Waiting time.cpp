8811293de48108b80785660ea5a10ae7abdd1595
Harsh Koli <113970702+Harshkoli121@users.noreply.github.com>
2023-01-05 22:28:16 +0530

    Create Waiting time.cpp

---
+++ CODE CHEF/Waiting time.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,k;
+	    cin>>k>>n;
+	        cout<<7*k-n<<endl;
+	    
+	}
+	return 0;
+}
