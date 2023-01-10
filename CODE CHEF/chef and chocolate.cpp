d1cecfb41d4b7a14c4f7fd915e3915efb9030a67
Harsh Koli <113970702+Harshkoli121@users.noreply.github.com>
2023-01-10 23:21:04 +0530

    Create chef and chocolate.cpp

---
+++ CODE CHEF/chef and chocolate.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	  
+	    cout<<abs(a-b)*c<<endl;
+	}
+	return 0;
+}
