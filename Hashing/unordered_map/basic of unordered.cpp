309305dcef3193427e5dbbea1638b74b05fe0fa7
Harsh Koli <113970702+Harshkoli121@users.noreply.github.com>
2023-01-12 12:33:28 +0530

    Update basic of unordered.cpp

--- Hashing/unordered_map/basic of unordered.cpp
+++ Hashing/unordered_map/basic of unordered.cpp
@@ -1,1 +1,17 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,number;
+    cin>>n;
+    string name;
+    unordered_map<string,int>marks;
+    for(int i=0;i<n;i++)
+    {
+        cin>>name>>number;
+        marks[name]=number;
+    }
+    cout<<"marks of ashutosh"<<marks["ashutosh"]<<endl;
+    cout<<"marks of ayush"<<marks["ayush"]<<endl;
+}
